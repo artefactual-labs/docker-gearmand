a6d0b2b7bdde5c4c674a6016361e860fcbacbe49
Mararok <mararok@gmail.com>
2018-09-15 11:51:21 +0200

    generating config_file from ENV vars
    - removed copy local config file
    - added install bash
    - added support for config options values from ENV with default values
    - added support for MYSQL_PASSWORD reading from file(Docker Secrets)

--- 1.1.18/docker-entrypoint.sh
+++ 1.1.18/docker-entrypoint.sh
@@ -1,9 +1,72 @@
-#!/bin/sh
-set -e
+#!/bin/bash
+set -eo pipefail
+CONFIG_FILE='/etc/gearmand.conf'
+VERBOSE=${VERBOSE:-INFO}
+QUEUE_TYPE=${QUEUE_TYPE:-builtin}
+THREADS=${THREADS:-4}
+JOB_RETRIES=${JOB_RETRIES:-0}
+KEEPALIVE=${KEEPALIVE:-0}
+KEEPALIVE_IDLE=${KEEPALIVE_IDLE:-30}
+KEEPALIVE_INTERVAL=${KEEPALIVE_IDLE:-10}
+KEEPALIVE_COUNT=${KEEPALIVE_COUNT:-5}
+
+# usage: file_env VAR [DEFAULT]
+#    ie: file_env 'XYZ_DB_PASSWORD' 'example'
+# (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
+#  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
+file_env() {
+	local var="$1"
+	local fileVar="${var}_FILE"
+	local def="${2:-}"
+	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+		echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+		exit 1
+	fi
+	local val="$def"
+	if [ "${!var:-}" ]; then
+		val="${!var}"
+	elif [ "${!fileVar:-}" ]; then
+		val="$(< "${!fileVar}")"
+	fi
+	export "$var"="$val"
+	unset "$fileVar"
+}
 
 # first arg is `-f` or `--some-option`
 if [ "${1#-}" != "$1" ]; then
 	set -- gearmand "$@"
+fi
+
+cat <<__CONFIG_CONTENT__ > "${CONFIG_FILE}"
+--listen=0.0.0.0
+--port=4730
+--log-file=stderr
+--verbose=${VERBOSE}
+--queue-type=${QUEUE_TYPE}
+--threads=${THREADS}
+--job-retries=${JOB_RETRIES}
+__CONFIG_CONTENT__
+
+if [[ ${KEEPALIVE} != '0' ]]; then
+cat <<__CONFIG_CONTENT__ >> "${CONFIG_FILE}"
+--keepalive
+--keepalive-idle=${KEEPALIVE_IDLE}
+--keepalive-interval=${KEEPALIVE_INTERVAL}
+--keepalive-count=${KEEPALIVE_COUNT}
+__CONFIG_CONTENT__
+fi
+
+if [[ "$QUEUE_TYPE" == 'mysql' ]]; then
+file_env 'MYSQL_PASSWORD'
+cat <<__CONFIG_CONTENT__ >> "${CONFIG_FILE}"
+--mysql-host=${MYSQL_HOST:-localhost}
+--mysql-port=${MYSQL_PORT:-3306}
+--mysql-user=${MYSQL_USER:-root}
+--mysql-db=${MYSQL_DB:-Gearmand}
+--mysql-table=${MYSQL_TABLE:-gearman_queue}
+--mysql-password=${MYSQL_PASSWORD}
+__CONFIG_CONTENT__
+
 fi
 
 exec "$@"
